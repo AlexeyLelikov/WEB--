9a6d193cefe3bdfef8a33f64889d01d44fb804dc
AlexeyLelikov <alexeylelikove@gmail.com>
2023-02-19 11:25:09 +0300

    module 2 lesson 2

---
+++ модуль 2/Занятие 2/app.py
@@ -0,0 +1,17 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+list_menu = [ "/about" , "/person" , "/home"]
+
+@app.route("/")
+def index():
+    return render_template("index.html", title = "Про Flask", menu = list_menu)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
