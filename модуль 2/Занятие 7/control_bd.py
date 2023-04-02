2049676b1ce51bf07ff49391cbdf0b6d92958710
AlexeyLelikov <alexeylelikove@gmail.com>
2023-04-02 11:11:53 +0300

    update lesson 7

--- модуль 2/Занятие 7/control_bd.py
+++ модуль 2/Занятие 7/control_bd.py
@@ -4,10 +4,36 @@ with sq.connect("saper.db") as con:
     cur = con.cursor()
 
     cur.execute("DROP TABLE IF EXISTS users")
-    #cur.execute("""CREATE TABLE IF NOT EXISTS users (
-     #           name TEXT,
-     #           sex TEXT,
-      #          old INTEGER,
-      #          score INTEGER
-     #           )
-      #          """)
+    cur.execute("""CREATE TABLE IF NOT EXISTS users (
+               name TEXT NOT NULL,
+               sex TEXT,
+               old INTEGER DEFAULT 18,
+               score INTEGER DEFAULT 0
+               )
+              """)
+    
+    cur.execute("""INSERT INTO users VALUES (
+                'Михаил',
+                'муж',
+                19,
+                1000
+                )
+                """)
+    
+    cur.execute("""INSERT INTO users VALUES (
+                'Cаша',
+                'жен',
+                20,
+                1500
+                )
+                """)
+    
+    cur.execute("SELECT * FROM users")
+
+    result = cur.fetchall()
+    print(result)
+
+    
+
+    
+
